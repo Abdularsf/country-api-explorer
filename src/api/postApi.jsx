dbd113d89666ba75ec7f58aa516e8bf3e0e406c8
Abdul Rahman <abdularsf147@gmail.com>
2026-02-14 21:18:29 +0530

    country page

--- src/api/postApi.jsx
+++ src/api/postApi.jsx
@@ -1,7 +1,9 @@
-const api = api.create({
+import axios from "axios";
+
+const api = axios.create({
     baseURL: "https://restcountries.com/v3.1",
 })
 
-const getCountryData = () =>{
+export const getCountryData = () =>{
     return api.get("/all?fields=name,population,region,capital,flags");
 }
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,3 +1,31 @@
-export const Country = () =>{
-    return <h1>Country</h1>
+import { useEffect, useState, useTransition } from "react"
+import { getCountryData } from "../api/postApi"
+import { Loader } from "../components/UI/Loader"
+
+export const Country = () => {
+    const [isPending, startTransition] = useTransition()
+    const [contries, setCountries] = useState([])
+
+    useEffect(() => {
+        startTransition(async () => {
+            const res = await getCountryData();
+            setCountries(res);
+        })
+    }, [])
+
+    if (isPending) return <Loader />
+
+    console.log(contries);
+
+    return (
+        <section className="country-section">
+            <div className="grid grid-four-cols">
+                {
+                    setCountries.map((curCountry) => {
+                        return <CountryCard country = {curCountry}/>
+                    })
+                }
+            </div>
+        </section>
+    )
 }
