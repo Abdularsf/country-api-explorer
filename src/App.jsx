1be0c6572093453473941bab2f7eff478a2aa550
Abdul Rahman <abdularsf147@gmail.com>
2026-02-14 16:31:42 +0530

    component setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,35 @@ import { Home } from './pages/Home'
 import { About } from './pages/About'
 import { Contact } from './pages/Contact'
 import { Country } from './pages/Country'
+import { AppLayout } from './components/Layout/AppLayout'
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<Home />,
-  },
-  {
-    path:"about",
-    element:<About />,
-  },
-  {
-    path:"country",
-    element:<Country />,
-  },
-  {
-    path:"contact",
-    element:<Contact />,
+    path: "/",
+    element: <AppLayout/>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "country",
+        element: <Country />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      }
+    ]
   }
 ])
 
-const App = () =>{
+const App = () => {
   return <RouterProvider router={router}></RouterProvider>
 }
- 
+
 export default App
