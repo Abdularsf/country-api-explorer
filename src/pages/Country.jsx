0b9f3fe105531d2dd553d72a53cf8c1bf3c96d45
Abdul Rahman <abdularsf147@gmail.com>
2026-02-16 00:01:54 +0530

    serchFilter

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -2,10 +2,13 @@ import { useEffect, useState, useTransition } from "react"
 import { getCountryData } from "../api/postApi"
 import { Loader } from "../components/UI/Loader"
 import { CountryCard } from "../components/Layout/CountryCard"
+import { SerchFilter } from "../components/UI/SearchFilter"
 
 export const Country = () => {
     const [isPending, startTransition] = useTransition()
-    const [contries, setCountries] = useState([])
+    const [countries, setCountries] = useState([])
+    const [search, setSearch] = useState();
+    const [filter, setFilter] = useState("all");
 
     useEffect(() => {
         startTransition(async () => {
@@ -16,14 +19,36 @@ export const Country = () => {
 
     if (isPending) return <Loader />
 
-    console.log(contries);
+    const searchCountry = (country) => {
+        if (search) {
+            return country.name.common.toLowerCase().includes(search.toLowerCase());
+        }
+        return country;
+    }
+
+    const filterRegion = (country) => {
+        if (filter === "all") return country;
+        return country.region === filter;
+    };
+
+    const filterCountries = countries.filter((country) => searchCountry(country) && filterRegion(country));
+
+    console.log(countries);
 
     return (
         <section className="country-section">
+            <SerchFilter
+                search={search}
+                setSearch={setSearch}
+                filter={filter}
+                setFilter={setFilter}
+                countries={countries}
+                setCountries={setCountries}
+            />
             <ul className="grid grid-four-cols">
                 {
-                    contries.map((curCountry,index) => {
-                        return <CountryCard country = {curCountry} key = {index}/>
+                    filterCountries.map((curCountry, index) => {
+                        return <CountryCard country={curCountry} key={index} />
                     })
                 }
             </ul>
